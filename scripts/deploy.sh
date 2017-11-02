3bd4370e85e827c6e5b4c1ffb8b64336ef067ea8
akhilvijayan05 <akhilvijayan05@gmail.com>
2017-11-03 03:15:18 +0530

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 scp -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ~/JenkinsKnolx/JenkinsProject/scripts/remote-deploy.sh ec2-user@18.221.171.188:~/
 
-ssh -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ec2-user@18.221.171.188 ~/remote-deploy.sh
+ssh -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ec2-user@18.221.171.188 remote-deploy.sh
