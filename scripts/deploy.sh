0ae61704d37d1b229379142b666e44a5c6f7ab9b
akhilvijayan05 <akhilvijayan05@gmail.com>
2017-11-03 03:10:25 +0530

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-scp -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem scripts/remote-deploy.sh ec2-user@18.221.171.188:~/
+scp -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ~/JenkinsKnolx/JenkinsProject/scripts/remote-deploy.sh ec2-user@18.221.171.188:~/
 
-ssh -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ec2-user@18.221.171.188 'bash -s' < ~/JenkinKnox/JenskinProject/scripts/remote-deploy.sh
+ssh -i /home/knoldus/JenkinsKnolx/JenkinsProjectKey.pem ec2-user@18.221.171.188 ~/remote-deploy.sh
