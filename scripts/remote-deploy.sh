e0c30effed298a6f95fc85c49681dde1f6b5cade
akhilvijayan05 <akhilvijayan05@gmail.com>
2017-11-03 11:36:31 +0530

    updated

--- scripts/remote-deploy.sh
+++ scripts/remote-deploy.sh
@@ -16,8 +16,8 @@ sudo docker stop $CONTAINER_NAME || echo "Container already stopped"
 
 sudo docker rm -f $CONTAINER_NAME || echo "Container already removed"
 
-sudo docker login -u $USER_NAME -p $PASSWORD 
+sudo docker login -u $USER_NAME -p $PASSWORD
 
 sudo docker pull $USER_NAME/$IMAGE_NAME:$IMAGE_TAG
 
-sudo docker run -d --name $CONTAINER_NAME -p $ACTIVE_PORT:$ACTIVE_PORT $IMAGE_NAME:$IMAGE_TAG
+sudo docker run -d --name $CONTAINER_NAME -p $ACTIVE_PORT:$ACTIVE_PORT $USER_NAME/$IMAGE_NAME:$IMAGE_TAG
